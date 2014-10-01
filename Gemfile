f600a6c3b963b804f83944070d010c19aa4836f1
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-01 09:33:42 -0500

    fixed RMagick

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '3.2.19'
 
 gem 'pg'
 gem 'carrierwave'
-gem 'rmagick'
+gem 'rmagick', :require => "RMagick"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-  attr_accessible :name, :photo
-  belongs_to :users
+  attr_accessible :name, :photo, :location, :budget
+  belongs_to :user
   mount_uploader :photo, PhotoUploader
 end
