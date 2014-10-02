3a7c0c1d2703b11a77fccb2ebf31e5e5c2ac4251
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-10-01 21:53:21 -0500

    added controller methods and resource routes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,37 @@
 class ListingsController < ApplicationController
+  
+  def index
+    @listings = Listing.all
+  end
+  
+  def show
+    @listing = Listing.find(params[:id])
+  end
+  
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+  
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update_attributes(params[:listing])
+  end
+  
+  def delete
+    @listing = Listing.find(params[:id]).destroy
+  end
+  
+  def new
+    @listing = Listing.new
+  end
+  
+  def create
+    @listing = Listing.new(params[:listing])
+    if @listing.save
+      redirect_to listings_path
+    else
+      render "new"
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+  end
+  
+  def delete
+    @user = User.find(params[:id]).destroy
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:listing])
+    if @user.save
+      redirect_to users_path
+    else
+      render "new"
+    end
+  end
+
 end
